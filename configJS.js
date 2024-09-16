e9103a572647bce3d347b2b8d2f7fffbbdc0f1f6
marcos-v-costa <marcoscosta.marq@gmail.com>
2024-09-16 17:48:33 -0300

    inclusão de sidebar, mudanças nos posicionamentos e coisas a fazer

--- configJS.js
+++ configJS.js
@@ -1,24 +1,4 @@
-function switchModal() {
-  const modal = document.querySelector(".modal");
-  const actualStyle = modal.style.display;
-  if (actualStyle == "block") {
-    modal.style.display = "none";
-  } else {
-    modal.style.display = "block";
-  }
-}
-
-const btn = document.querySelector(".botao-excluir");
-btn.addEventListener("click", switchModal);
-
-window.onclick = function (event) {
-  const modal = document.querySelector(".modal");
-  if (event.target == modal) {
-    switchModal();
-  }
-};
-
-const iconButton = document.querySelector("#iconOlho");
+const iconButton = document.querySelector("#iconEye");
 const typePass = document.querySelector("#password");
 
 iconButton.addEventListener("click", olhoSenha);
@@ -33,22 +13,95 @@ function olhoSenha() {
   }
 }
 
-const switchModalConfirmar = () => {
-  const modalConfirmar = document.querySelector(".modalConfirmar");
-  const actualStyleConfirmar = modalConfirmar.style.display;
-  if (actualStyleConfirmar == "block") {
-    modalConfirmar.style.display = "none";
+function switchModal() {
+  const modal = document.querySelector(".modal");
+  const actualStyle = modal.style.display;
+  if (actualStyle == "block") {
+    modal.style.display = "none";
   } else {
-    modalConfirmar.style.display = "block";
+    modal.style.display = "block";
   }
-};
+}
 
-const btnConfirmar = document.querySelector(".botao-confirmar");
-btnConfirmar.addEventListener("click", switchModalConfirmar);
+const btn = document.querySelector(".delete-button");
+btn.addEventListener("click", switchModal);
 
 window.onclick = function (event) {
-  const modalConfirmar = document.querySelector(".modalConfirmar");
-  if (event.target == modalConfirmar) {
-    switchModalConfirmar();
+  const modal = document.querySelector(".modal");
+  if (event.target == modal) {
+    switchModal();
   }
 };
+
+const apelido = document.getElementById("surname");
+const name = document.getElementById("nome");
+const lastName = document.getElementById("sobrenome");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+function setDataUser(surname, nameUser, lastNameUser, emailUser, passwordUser) {
+  apelido.value = surname;
+  name.value = nameUser;
+  lastName.value = lastNameUser;
+  email.value = emailUser;
+  password.value = passwordUser;
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const userID = {
+    _id: "66e4e9d5d51ddb637ef723bf",
+  };
+
+  try {
+    const response = await fetch("http://localhost:3000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userID),
+    });
+
+    const data = await response.json();
+    console.log(data.surname);
+
+    const messageDiv = document.getElementById("message");
+    if (response.ok) {
+      setDataUser(
+        data.surname,
+        data.name,
+        data.last_name,
+        data.email,
+        data.password
+      );
+    }
+  } catch (error) {
+    console.error("Erro na requisição:", error);
+    const messageDiv = document.getElementById("message");
+    messageDiv.textContent = "Erro na requisição.";
+    messageDiv.style.color = "red";
+  }
+});
+
+const buttonConfirm = document
+  .getElementById("confirmButton")
+  .addEventListener("click", async () => {
+    const userUpdate = {
+      _id: "66e4e9d5d51ddb637ef723bf",
+      name: name.value,
+      last_name: lastName.value,
+      surname: apelido.value,
+      email: email.value,
+      password: password.value,
+    };
+    try {
+      const response = await fetch("http://localhost:3000/user", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userUpdate),
+      });
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+    }
+  });
